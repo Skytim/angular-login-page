7b8931f92e634cc985d22370024367c9d1f5d65d
Tim Tsai <sky22357168@gmail.com>
2021-06-28 17:23:13 +0800

    Update package

--- login-page/src/app/services/auth.service.ts
+++ login-page/src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { isDevMode } from '@angular/core';
 @Injectable({
@@ -10,14 +10,21 @@ export class AuthService {
     if (isDevMode()) {
       this.config = 'http://localhost:3000/'
     } else {
-      this.config = 'https://floating-sea-66924.herokuapp.com/';
+      this.config = 'https://thawing-wave-52886.herokuapp.com/';
     }
+    this.config = 'https://thawing-wave-52886.herokuapp.com/';
     this.config += 'api/login'
   }
 
   login(Acc: string, UserID: string, Pw: string) {
 
-    return this.http.post(this.config, { Acc, UserID, Pw });
+    return this.http.post(this.config, { Acc, UserID, Pw }, {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+        'Access-Control-Max-Age': '86400'
+      })
+    });
   }
 
 
