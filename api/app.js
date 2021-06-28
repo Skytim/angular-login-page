39052b11794a72062709278ee9a4a7acc1e61516
Tim Tsai <sky22357168@gmail.com>
2021-06-28 18:52:23 +0800

    Login Service

--- api/app.js
+++ api/app.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 var login = require('./routes/login');
 var PORT = process.env.PORT || 5000
 var app = express();
 app.use(cors());
+app.use( bodyParser.json() );       
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
+
 
 app.use('/api/login', login);
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
